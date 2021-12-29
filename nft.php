24f23077d6cc8e9adea1c9f9f683b676586fe854
bintangfadillah <39184428+star2112@users.noreply.github.com>
2021-12-29 20:14:44 +0700

    Update nft.php

--- nft.php
+++ nft.php
@@ -8,7 +8,7 @@
     <section class="py-5 text-center container">
       <div class="row py-lg-5">
         <div class="col-lg-6 col-md-8 mx-auto">
-          <h1 class="fw-light"><a href="index.html">Dunia IT</a></h1>
+          <h1 class="fw-light"><a href="index.php">Dunia IT</a></h1>
           <p class="lead text-muted">
             Berisi informasi seputar dunia IT terbaru
           </p>
