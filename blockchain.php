e9349d90f1f946ab6976a9ebb4684da40c33c7c1
bintangfadillah <39184428+star2112@users.noreply.github.com>
2021-12-29 19:47:28 +0700

    Add files via upload

---
+++ blockchain.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link rel="stylesheet" href="bootstrap.css" />
+    <title>Info IT</title>
+  </head>
+  <body>
+    <section class="py-5 text-center container">
+      <div class="row py-lg-5">
+        <div class="col-lg-6 col-md-8 mx-auto">
+          <h1 class="fw-light"><a href="index.html">Dunia IT</a></h1>
+          <p class="lead text-muted">
+            Berisi informasi seputar dunia IT terbaru
+          </p>
+        </div>
+      </div>
+    </section>
+    <div class="d-flex justify-content-center align-items-center">
+      <img src="metaverse.jpg" style="width: 40%" />
+    </div>
+
+    <div class="container">
+      <h3 class="fw-light">Apa itu blockchain</h3>
+      <p>
+        Dilansir dari buku Blockchain for Dummies oleh Manav Gupta dijelaskan,
+        blockchain mulanya dikembangkan untuk memenuhi kebutuhan akan sistem
+        yang efisien, hemat biaya, andal, dan aman untuk melakukan dan mencatat
+        sebuah transaksi keuangan. Blockchain mulanya digunakan pada bitcoin
+        yang dikembangkan pada tahun 2009 oleh Satoshi Nakamoto. Tak seperti
+        uang tradisional yang diterbitkan oleh bank sentral, bitcoin tidak
+        memiliki otoritas sentral dan tidak ada pihak yang mengontrol. Ketimbang
+        bergantung pada otoritas sentral untuk mengawasi, melakukan verifikasi
+        dan menyetujui transaksi serta mengelola pasokan uang, bitcoin
+        diaktifkan oleh jaringan komputer peer to peer. Dilansir dari
+        Investopedia, blockchain adalah sebuah pusat data terdistribusi yang
+        dibagikan pada setiap titik yang ada pada jaringan komputer. Sebagai
+        data base atau pusat data, blockchain berisi beragam informasi dalam
+        format digital. Informasi yang ada di dalamnya tergantung pada jenis
+        penggunaan blockchain. Misalnya saja pada bitcoin, informasi yang
+        terdapat di dalamnya terkait dengan detil transaksi, jumlah koin,
+        pemilih koin, hingga penerima koin. Dengan demikian, blockchain
+        berfungsi untuk menjaga catatan mengenai transaksi yang aman dan
+        terdesentralisasi. Apa yang membedakan blockchain dengan pendataan
+        konvensional? Seperti yang dijelaskan sebelumnya, sistem blockchain
+        membuat sebuah catatan transaksi menjadi terdesentralisasi. Blockchain
+        adalah sebuah buku kas besar (ledger) yang terdistribusi. Teknologi
+        inilah yang membuat pencatatan terjadi di banyak komputer atau yang
+        dikenal dengan nodes atau simpul. Setiap pengguna blockchain bisa
+        menjadi simpul-simpul yang saling terhubung, namun untuk
+        pengoperasiannya, membuautuhkan tenaga komputer yang sangat besar.
+        Simpul-simpul tersebutlah yang kemudian melakukan verifikasi,
+        menyetujui, dan mencatatkan data di ledger.
+      </p>
+    </div>
+    <footer></footer>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link rel="stylesheet" href="bootstrap.css" />
+    <title>Info IT</title>
+  </head>
+  <body>
+    <section class="py-5 text-center container">
+      <div class="row py-lg-5">
+        <div class="col-lg-6 col-md-8 mx-auto">
+          <h1 class="fw-light"><a href="index.html">Dunia IT</a></h1>
+          <p class="lead text-muted">
+            Berisi informasi seputar dunia IT terbaru
+          </p>
+        </div>
+      </div>
+    </section>
+    <div class="album py-5 bg-light">
+      <div class="container">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+          <div class="col">
+            <div class="card shadow-sm">
+              <div class="card-body">
+                <p class="card-text">
+                  <h2 class="fw-light just">Metaverse</h2>
+                </p>
+                <img src="metaverse.jpg" style="width: 100%; margin-bottom: 0.75rem;">
+                <div class="d-flex justify-content-between align-items-center">
+                  <a href="metaverse.html" class="btn btn-sm btn-outline-secondary">Lihat selengkapnya</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col">
+            <div class="card shadow-sm">
+              <div class="card-body">
+                <p class="card-text">
+                  <h2 class="fw-light just">NFT</h2>
+                </p>
+                <img src="metaverse.jpg" style="width: 100%; margin-bottom: 0.75rem;">
+                <div class="d-flex justify-content-between align-items-center">
+                  <a href="nft.html" class="btn btn-sm btn-outline-secondary">Lihat selengkapnya</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col">
+            <div class="card shadow-sm">
+              <div class="card-body">
+                <p class="card-text">
+                  <h2 class="fw-light just">Blockchain</h2>
+                </p>
+                <img src="metaverse.jpg" style="width: 100%; margin-bottom: 0.75rem;">
+                <div class="d-flex justify-content-between align-items-center">
+                  <a href="blockchain.html" class="btn btn-sm btn-outline-secondary">Lihat selengkapnya</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        
+        </div>
+      </div>
+    </div>
+    <footer>
+
+    </footer>
+  </body>
+</html>
